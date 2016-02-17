e7282b1c749b7c590a117920ded63f93bf06ffe7
Silvian <silvian@localhost.localdomain>
2016-02-17 21:58:45 +0000

    created a config.ini file in order to make the streamkit monitor fully generic and configurable

--- get-streamkit-json.php
+++ get-streamkit-json.php
@@ -1,8 +1,10 @@
 <?php
+$config_array = parse_ini_file("config.ini");
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://play.streamkit.tv/content/channel/us_jesus_market/live/us_jesus_market.player.json?jsoncallback=?');
+curl_setopt($ch, CURLOPT_URL, $config_array["URL"]);
 $result = curl_exec($ch);
 curl_close($ch);
 
